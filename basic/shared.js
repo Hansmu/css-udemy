61c80f6151c23d11422a7a1fa320dab4cf54c3fe
hansmu <hans.miikael@gmail.com>
2020-07-15 09:38:22 +0300

    Add JS display example

--- basic/shared.js
+++ basic/shared.js
@@ -2,15 +2,36 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButton = document.querySelectorAll('.plan button');
 const closeModalButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', () => {
-        modal.style.display = 'initial';
-        backdrop.style.display = 'initial';
+        /* When setting display directly on the styles object, then the
+        * values get applied as inline styles. Also, instead of being
+        * kebab case, the style is in camelCase. */
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-closeModalButton.addEventListener('click', () => {
-    modal.style.display = '';
-    backdrop.style.display = '';
+if (closeModalButton) {
+    closeModalButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+}
+
+backdrop.addEventListener('click', () => {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
